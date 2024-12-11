165b94a17e67397019ddbe6b53c0ad485d78a28d
Jendrik Johannes <jendrik.johannes@gmail.com>
2024-12-11 11:35:25 +0100

    chore: use hiero-gradle-conventions to build hiero-gradle-conventions
    Signed-off-by: Jendrik Johannes <jendrik.johannes@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,12 @@
 // SPDX-License-Identifier: Apache-2.0
 plugins {
+    id("org.hiero.gradle.module.gradle-plugin")
     `kotlin-dsl`
-    id("signing")
-    id("com.gradle.plugin-publish") version "1.3.0"
-    id("com.diffplug.spotless") version "6.25.0"
 }
-
-version = "0.1.1"
 
 group = "org.hiero.gradle"
 
 description = "Gradle convention plugins used by Hiero projects"
-
-java { toolchain.languageVersion = JavaLanguageVersion.of(17) }
 
 dependencies {
     implementation("com.adarshr:gradle-test-logger-plugin:4.0.0")
@@ -32,6 +26,12 @@ dependencies {
     implementation("org.gradlex:java-module-dependencies:1.8")
     implementation("org.gradlex:jvm-dependency-conflict-resolution:2.1.2")
     implementation("org.gradlex:reproducible-builds:1.0")
+
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom:${embeddedKotlinVersion}"))
+
+    testImplementation(platform("org.junit:junit-bom:5.11.3"))
+    testImplementation("org.assertj:assertj-core:3.26.3")
+    testImplementation("org.junit.jupiter:junit-jupiter-params")
 }
 
 gradlePlugin {
@@ -59,80 +59,16 @@ gradlePlugin {
     plugins.configureEach { displayName = name }
 }
 
-publishing.publications.withType<MavenPublication>().configureEach {
-    pom {
-        url = "https://hiero.org/"
-        inceptionYear = "2024"
-        description = project.description
-        name = project.name
-        organization {
-            name = "Hiero - a Linux Foundation Decentralized Trust project"
-            url = "https://hiero.org/"
-        }
-
-        val repoName = project.name
-        issueManagement {
-            system = "GitHub"
-            url = "https://github.com/hiero-ledger/$repoName/issues"
-        }
-
-        licenses {
-            license {
-                name = "Apache License, Version 2.0"
-                url = "https://raw.githubusercontent.com/hiero-ledger/$repoName/main/LICENSE"
-            }
-        }
-
-        scm {
-            connection = "scm:git:git://github.com/hiero-ledger/$repoName.git"
-            developerConnection = "scm:git:ssh://github.com:hiero-ledger/$repoName.git"
-            url = "https://github.com/hiero-ledger/$repoName"
-        }
-
-        developers {
-            developer {
-                id = "Release Engineering Team"
-                name = "Release Engineering Team"
-                email = "release-engineering@hiero.org"
-                organization = "Hiero - a Linux Foundation Decentralized Trust project"
-                organizationUrl = "https://hiero.org/"
-            }
-        }
-    }
-}
-
-val publishSigningEnabled =
-    providers.gradleProperty("publishSigningEnabled").getOrElse("false").toBoolean()
-
-if (publishSigningEnabled) {
-    signing {
-        sign(publishing.publications)
-        useGpgCmd()
-    }
-}
-
-testing {
-    @Suppress("UnstableApiUsage")
-    suites.named<JvmTestSuite>("test") {
-        useJUnitJupiter()
-        dependencies {
-            implementation("org.junit.jupiter:junit-jupiter-params")
-            implementation("org.assertj:assertj-core:3.26.3")
-        }
-        // If success, delete all test projects
-        targets.all { testTask { doLast { File("build/test-projects").deleteRecursively() } } }
-    }
+tasks.test {
+    // If success, delete all test projects
+    doLast { File("build/test-projects").deleteRecursively() }
 }
 
 spotless {
+    // Format '*.kts' and '*.kt' in 'src/main/kotlin'
     val header = "// SPDX-License-Identifier: Apache-2.0\n"
     val delimiter =
         "(import|package|plugins|pluginManagement|dependencyResolutionManagement|repositories|tasks|allprojects|subprojects|buildCache|version)"
-
-    kotlinGradle {
-        ktfmt().kotlinlangStyle()
-        licenseHeader(header, delimiter)
-    }
     kotlin {
         ktfmt().kotlinlangStyle()
         targetExclude("build/**")
