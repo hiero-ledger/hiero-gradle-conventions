b54c67a27989c3cee22e6e90339cf40213f5cd65
Jendrik Johannes <jendrik.johannes@gmail.com>
2024-12-11 11:58:06 +0100

    chore: use hiero-gradle-conventions to build hiero-gradle-conventions (#36)
    Signed-off-by: Jendrik Johannes <jendrik.johannes@gmail.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,15 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
-plugins { id("com.gradle.develocity") version "3.18.2" }
-
-rootProject.name = "hiero-gradle-conventions"
-
-develocity {
-    buildScan {
-        termsOfUseUrl = "https://gradle.com/help/legal-terms-of-use"
-        termsOfUseAgree = "yes"
-        // Enable Gradle Build Scan only with explicit '--scan'
-        publishing.onlyIf { false }
-    }
+plugins {
+    id("org.hiero.gradle.build") version "0.1.1"
+    `kotlin-dsl` apply false
 }
 
 dependencyResolutionManagement { @Suppress("UnstableApiUsage") repositories.gradlePluginPortal() }
