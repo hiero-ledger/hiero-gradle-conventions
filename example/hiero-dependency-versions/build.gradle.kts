f98887ee9429123b90c6effe9258a269c9eecce5
Jendrik Johannes <jendrik.johannes@gmail.com>
2025-01-16 11:40:01 +0100

    chore: extend example project
    Signed-off-by: Jendrik Johannes <jendrik.johannes@gmail.com>

--- example/hiero-dependency-versions/build.gradle.kts
+++ example/hiero-dependency-versions/build.gradle.kts
@@ -2,10 +2,14 @@
 plugins {
     id("org.hiero.gradle.base.lifecycle")
     id("org.hiero.gradle.base.jpms-modules")
+    id("org.hiero.gradle.check.spotless")
+    id("org.hiero.gradle.check.spotless-kotlin")
 }
 
 dependencies.constraints {
     api("com.fasterxml.jackson.core:jackson-databind:2.16.0") {
         because("com.fasterxml.jackson.databind")
     }
+
+    api("org.junit.jupiter:junit-jupiter-api:5.10.2") { because("org.junit.jupiter.api") }
 }
