8b18589c91a5a92b4db5823f70b7caf6699a9f88
Jendrik Johannes <jendrik.johannes@gmail.com>
2025-04-17 11:23:28 +0200

    build: update 'java-module-dependencies' and 'java-module-testing'
    Signed-off-by: Jendrik Johannes <jendrik.johannes@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,8 +29,8 @@ dependencies {
     implementation("me.champeau.jmh:jmh-gradle-plugin:0.7.3")
     implementation("net.swiftzer.semver:semver:2.1.0")
     implementation("org.gradlex:extra-java-module-info:1.12")
-    implementation("org.gradlex:java-module-dependencies:1.8.1")
-    implementation("org.gradlex:java-module-testing:1.6.1")
+    implementation("org.gradlex:java-module-dependencies:1.9")
+    implementation("org.gradlex:java-module-testing:1.7")
     implementation("org.gradlex:jvm-dependency-conflict-resolution:2.2")
     implementation("org.gradlex:reproducible-builds:1.0")
 
