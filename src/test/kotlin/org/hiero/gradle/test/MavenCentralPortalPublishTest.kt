347f4c0104f2e43368f5d2c7d6b26e6297a86b2f
Jendrik Johannes <jendrik.johannes@gmail.com>
2025-05-13 08:23:12 +0200

    chore: add test for new Maven Central Portal publishing
    Signed-off-by: Jendrik Johannes <jendrik.johannes@gmail.com>

---
+++ src/test/kotlin/org/hiero/gradle/test/MavenCentralPortalPublishTest.kt
@@ -0,0 +1,111 @@
+// SPDX-License-Identifier: Apache-2.0
+package org.hiero.gradle.test
+
+import org.assertj.core.api.Assertions.assertThat
+import org.gradle.testkit.runner.TaskOutcome
+import org.hiero.gradle.test.fixtures.GradleProject
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class MavenCentralPortalPublishTest {
+
+    lateinit var p: GradleProject
+
+    @BeforeEach
+    fun setup() {
+        p = GradleProject().withMinimalStructure()
+        p.withEnv(mapOf("NEXUS_USERNAME" to "foo", "NEXUS_PASSWORD" to "bar"))
+        p.aggregationBuildFile(
+            """
+            plugins { id("org.hiero.gradle.feature.publish-maven-central-aggregation") }
+            dependencies { published(project(":module-a")) }
+        """
+        )
+        p.moduleBuildFile("""plugins { id("org.hiero.gradle.module.library") }""")
+    }
+
+    @Test
+    fun `attempts to upload a single archive for non-snapshot releases`() {
+        val result = p.runAndFail("publishAggregationToCentralPortal")
+
+        // Creates the publication zip, but fails to upload due to bad credentials
+        assertThat(
+                p.file(
+                    "product/module-a/build/nmcp/m2Maven/org/example/module-a/1.0/module-a-1.0.jar"
+                )
+            )
+            .exists()
+        assertThat(
+                p.file(
+                    "product/module-a/build/nmcp/m2Maven/org/example/module-a/1.0/module-a-1.0.module"
+                )
+            )
+            .exists()
+        assertThat(result.output)
+            .contains(
+                "> Cannot deploy to maven central (status='401'): {\"error\":{\"message\":\"Invalid token\"}}"
+            )
+        assertThat(result.task(":module-a:zipMavenPublication")?.outcome)
+            .isEqualTo(TaskOutcome.SUCCESS)
+        assertThat(result.task(":aggregation:publishAggregationToCentralPortal")?.outcome)
+            .isEqualTo(TaskOutcome.FAILED)
+    }
+
+    @Test
+    fun `publishes each module directly for snapshot releases`() {
+        p.versionFile.writeText("1.0-SNAPSHOT")
+
+        // Finishes successfully even though we did not provide credentials
+        val result = p.runAndFail("publishAggregationToCentralPortal --offline")
+
+        // Does attempt actual publishing step of 'module-a' (fails only due to --offline)
+        assertThat(p.dir("product/module-a/build/nmcp/m2Maven")).isEmptyDirectory()
+        assertThat(result.output)
+            .contains(
+                "> No cached resource 'https://central.sonatype.com/repository/maven-snapshots/"
+            )
+        assertThat(result.task(":module-a:generateMetadataFileForMavenPublication")?.outcome)
+            .isEqualTo(TaskOutcome.SUCCESS)
+        assertThat(result.task(":module-a:generatePomFileForMavenPublication")?.outcome)
+            .isEqualTo(TaskOutcome.SUCCESS)
+        assertThat(result.task(":module-a:publishMavenPublicationToNmcpMavenRepository")?.outcome)
+            .isEqualTo(TaskOutcome.FAILED)
+    }
+
+    @Test
+    fun `does not perform auto-release when publishTestRelease=true`() {
+        // Modify some tasks to do nothing, because we cannot actually publish to Maven Central
+        p.aggregationBuildFile(
+            """
+            plugins { id("org.hiero.gradle.feature.publish-maven-central-aggregation") }
+            dependencies { published(project(":module-a")) }
+            tasks.publishAggregationToCentralPortal {
+                actions.clear()
+                doLast { println("publicationType=" + inputs.properties["publicationType"]) }
+            }
+        """
+        )
+        p.moduleBuildFile(
+            """
+            plugins { id("org.hiero.gradle.module.library") }
+            tasks.publishMavenPublicationToSonatypeRepository { actions.clear(); doLast {} }
+            """
+        )
+        val result = p.run("publishAggregationToCentralPortal -PpublishTestRelease=true")
+
+        assertThat(result.task(":module-a:publishMavenPublicationToNmcpMavenRepository")?.outcome)
+            .isEqualTo(TaskOutcome.SUCCESS)
+        assertThat(result.task(":module-a:zipMavenPublication")?.outcome)
+            .isEqualTo(TaskOutcome.SUCCESS)
+        assertThat(result.task(":aggregation:zipAggregation")?.outcome)
+            .isEqualTo(TaskOutcome.SUCCESS)
+        assertThat(result.output).contains("publicationType=USER_MANAGED")
+    }
+
+    @Test
+    fun `can use tasks task without issue if there are no publishing credentials`() {
+        val result = p.run("tasks")
+
+        assertThat(result.task(":tasks")?.outcome).isEqualTo(TaskOutcome.SUCCESS)
+    }
+}
