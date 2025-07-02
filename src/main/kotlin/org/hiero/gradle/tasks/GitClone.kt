4d859d6015864d59cdd78d22d9b5e3b984467706
Nana Essilfie-Conduah <nana@swirldslabs.com>
2025-07-02 09:39:11 -0400

    Add initial support for more robust git cloning
    Signed-off-by: Nana Essilfie-Conduah <nana@swirldslabs.com>

--- src/main/kotlin/org/hiero/gradle/tasks/GitClone.kt
+++ src/main/kotlin/org/hiero/gradle/tasks/GitClone.kt
@@ -32,6 +32,19 @@ abstract class GitClone : DefaultTask() {
 
     @get:Inject protected abstract val layout: ProjectLayout
 
+    @get:Input @get:Optional abstract val noCheckout: Property<Boolean>
+
+    @get:Input @get:Optional abstract val sparseClone: Property<Boolean>
+
+    @get:Input @get:Optional abstract val cloneDepth: Property<Int>
+
+    @get:Input @get:Optional abstract val filter: Property<String>
+
+    @get:Input @get:Optional abstract val sparseCheckoutPaths: Property<List<String>>
+
+    @get:Input @get:Optional abstract val reset: Property<Boolean>
+
+
     init {
         offline.set(startParameter.isOffline)
         // If a 'branch' is configured, the task is never up-to-date as it may change
@@ -49,31 +62,88 @@ abstract class GitClone : DefaultTask() {
             exec.exec {
                 if (!localClone.dir(".git").asFile.exists()) {
                     workingDir = localClone.asFile.parentFile
-                    commandLine("git", "clone", url.get(), "-q")
+                    // build clone command
+                    val gitCloneStringList: ArrayList<String> = arrayListOf("git", "clone")
+
+                    if (sparseClone.isPresent && sparseClone.get()) {
+                        gitCloneStringList.add("--sparse")
+                    }
+
+                    if (cloneDepth.isPresent) {
+                        gitCloneStringList.add("--depth=${cloneDepth.get()}")
+                    }
+
+                    if (noCheckout.isPresent && noCheckout.get()) {
+                        gitCloneStringList.add("--no-checkout")
+                    }
+
+                    if (filter.isPresent) {
+                        gitCloneStringList.add("--filter=${filter.get()}")
+                    }
+
+                    // add final url and quiet params
+                    gitCloneStringList.add(url.get())
+                    gitCloneStringList.add("-q")
+                    commandLine(gitCloneStringList)
                 } else {
                     workingDir = localClone.asFile
                     commandLine("git", "fetch", "-q")
                 }
             }
         }
-        if (tag.isPresent) {
+
+        // handle sparse checkout
+        if (sparseCheckoutPaths.isPresent) {
             exec.exec {
                 workingDir = localClone.asFile
-                commandLine("git", "checkout", tag.get(), "-q")
+                commandLine("git", "sparse-checkout", "init")
             }
+
+            // build spare-checkout set command
+            val gitSparseCheckoutSetStringList: ArrayList<String> =
+                arrayListOf("git", "sparse-checkout", "set")
+            sparseCheckoutPaths.get().forEach { path -> gitSparseCheckoutSetStringList.add(path) }
+
+            gitSparseCheckoutSetStringList.add("-q")
             exec.exec {
                 workingDir = localClone.asFile
-                commandLine("git", "reset", "--hard", tag.get(), "-q")
-            }
-        } else {
-            exec.exec {
-                workingDir = localClone.asFile
-                commandLine("git", "checkout", branch.get(), "-q")
-            }
-            exec.exec {
-                workingDir = localClone.asFile
-                commandLine("git", "reset", "--hard", "origin/${branch.get()}", "-q")
+                commandLine(gitSparseCheckoutSetStringList)
             }
         }
+
+        if (!reset.isPresent) {
+            // build checkout command
+            val gitCheckoutStringList: ArrayList<String> = arrayListOf("git", "checkout")
+
+            if (tag.isPresent) {
+                gitCheckoutStringList.add(tag.get())
+            } else {
+                gitCheckoutStringList.add("origin/${branch.get()}")
+            }
+
+            gitCheckoutStringList.add("-q")
+            exec.exec {
+                workingDir = localClone.asFile
+                commandLine(gitCheckoutStringList)
+            }
+        }
+        if (reset.isPresent && reset.get()) {
+            // build reset command
+            val gitResetStringList: ArrayList<String> = arrayListOf("git", "reset", "--hard")
+
+            if (tag.isPresent) {
+                gitResetStringList.add(tag.get())
+            } else {
+                gitResetStringList.add("origin/${branch.get()}")
+            }
+
+            gitResetStringList.add("-q")
+            exec.exec {
+                workingDir = localClone.asFile
+                commandLine(gitResetStringList)
+            }
+        }
+
+        println("Successfully cloned or updated $url to ${localClone.asFile.absolutePath}")
     }
 }
