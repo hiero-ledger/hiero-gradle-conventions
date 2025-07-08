980278b2a2070997e47c36982b06dd410b8edd25
Andrew Brandt <andrew.brandt@hashgraph.com>
2025-07-08 12:18:58 -0500

    chore: increase Maven Central publishing timeout to 1 hour (#247)
    Signed-off-by: Andrew Brandt <andrew.brandt@hashgraph.com>

--- src/main/kotlin/org.hiero.gradle.feature.publish-maven-central-aggregation.gradle.kts
+++ src/main/kotlin/org.hiero.gradle.feature.publish-maven-central-aggregation.gradle.kts
@@ -28,7 +28,11 @@ nmcpAggregation {
         username = providers.environmentVariable("NEXUS_USERNAME")
         password = providers.environmentVariable("NEXUS_PASSWORD")
         publishingType = if (publishTestRelease) "USER_MANAGED" else "AUTOMATIC"
-        validationTimeout = Duration.of(30, ChronoUnit.MINUTES)
+        validationTimeout =
+            Duration.of(
+                providers.gradleProperty("mavenCentralTimeout").getOrElse("60").toLong(),
+                ChronoUnit.MINUTES,
+            )
     }
 }
 
