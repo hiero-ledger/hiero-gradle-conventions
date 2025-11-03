f5ebcf570b0abf4b0acc41f9dabf9ab60f7ccc9c
Jendrik Johannes <jendrik.johannes@gmail.com>
2025-11-03 10:32:35 +0100

    build: remove implicit dependency from 'testFixtures' to 'main'
    Signed-off-by: Jendrik Johannes <jendrik.johannes@gmail.com>

--- src/main/kotlin/org.hiero.gradle.feature.test-fixtures.gradle.kts
+++ src/main/kotlin/org.hiero.gradle.feature.test-fixtures.gradle.kts
@@ -12,3 +12,12 @@ plugins {
     withVariantsFromConfiguration(configurations["testFixturesApiElements"]) { skip() }
     withVariantsFromConfiguration(configurations["testFixturesRuntimeElements"]) { skip() }
 }
+
+// Remove implicit dependency from 'testFixtures' to 'main', as we declare all dependencies
+// explicitly. This is necessary for the case where a module does not have a 'main' module-info.
+// https://github.com/gradle/gradle/blob/d9303339298e6206182fd1f5c7e51f11e4bdff30/subprojects/plugins/src/main/java/org/gradle/api/plugins/JavaTestFixturesPlugin.java#L68
+configurations {
+    testFixturesApi {
+        withDependencies { remove(find { it is ProjectDependency && it.name == project.name }) }
+    }
+}
