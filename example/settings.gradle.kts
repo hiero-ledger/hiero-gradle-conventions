3f182e606311a04ba282c516a6bde31c1dbf983d
Jendrik Johannes <jendrik.johannes@gmail.com>
2025-12-17 10:51:44 +0100

    build: increment version
    Signed-off-by: Jendrik Johannes <jendrik.johannes@gmail.com>

--- example/settings.gradle.kts
+++ example/settings.gradle.kts
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: Apache-2.0
-plugins { id("org.hiero.gradle.build") version "0.6.2" }
+plugins { id("org.hiero.gradle.build") version "0.6.3" }
 
 rootProject.name = "example-repository"
 
