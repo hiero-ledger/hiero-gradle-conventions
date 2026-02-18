41c0ec4b47970d5b7e7218ae3b69760e9f5dd633
Jendrik Johannes <jendrik@onepiece.software>
2026-02-18 09:20:36 +0100

    fix: handle listFiles() is null case (#411)
    Signed-off-by: Jendrik Johannes <jendrik@onepiece.software>

--- src/main/kotlin/org.hiero.gradle.feature.java-doc.gradle.kts
+++ src/main/kotlin/org.hiero.gradle.feature.java-doc.gradle.kts
@@ -8,7 +8,9 @@ tasks.javadoc {
     // Only enabled if there are Java sources in packages (subdirectories).
     // Needed, as the task fails if there is only a 'module-info.java' as the only source file.
     enabled =
-        sourceSets.main.get().java.srcDirs.any { it.listFiles().any { file -> file.isDirectory } }
+        sourceSets.main.get().java.srcDirs.any {
+            it.listFiles()?.any { file -> file.isDirectory } ?: false
+        }
     options {
         this as StandardJavadocDocletOptions
         tags(
