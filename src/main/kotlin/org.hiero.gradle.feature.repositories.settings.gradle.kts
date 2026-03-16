1e2698bbe4bac132f7701f7e020b0835fc24ded9
Nathan Klick <nathan@swirldslabs.com>
2026-03-16 03:46:38 -0500

    feat: add Hiero Besu maven proxy as primary repository (#431)
    Signed-off-by: Nathan Klick <nathan@swirldslabs.com>

--- src/main/kotlin/org.hiero.gradle.feature.repositories.settings.gradle.kts
+++ src/main/kotlin/org.hiero.gradle.feature.repositories.settings.gradle.kts
@@ -2,6 +2,9 @@
 dependencyResolutionManagement {
     @Suppress("UnstableApiUsage")
     repositories {
+        maven("https://artifacts.hashgraph.io/artifactory/hyperledger-besu-maven-external/") {
+            content { includeGroupByRegex("org\\.hyperledger\\..*") }
+        }
         maven("https://hyperledger.jfrog.io/artifactory/besu-maven") {
             content { includeGroupByRegex("org\\.hyperledger\\..*") }
         }
