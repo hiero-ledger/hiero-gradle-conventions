5035c5928f2e0d2f3342def61b6c360958df4ccd
Andrew Brandt <andrew.brandt@hashgraph.com>
2026-05-05 13:45:52 -0500

    chore: add ability to publish to different orgs
    Signed-off-by: Andrew Brandt <andrew.brandt@hashgraph.com>

--- src/main/kotlin/org.hiero.gradle.feature.publish-maven-repository.gradle.kts
+++ src/main/kotlin/org.hiero.gradle.feature.publish-maven-repository.gradle.kts
@@ -51,23 +51,29 @@ publishing.publications.withType<MavenPublication>().configureEach {
         }
 
         @Suppress("UnstableApiUsage") val repoName = isolated.rootProject.name
+        val gitHubOrg =
+            providers
+                .environmentVariable("GITHUB_REPOSITORY")
+                .map { it.substringBefore("/") }
+                .orElse(providers.gradleProperty("publishGitHubOrg"))
+                .orElse("hiero-ledger")
 
         issueManagement {
             system = "GitHub"
-            url = "https://github.com/hiero-ledger/$repoName/issues"
+            url = "https://github.com/${gitHubOrg.get()}/$repoName/issues"
         }
 
         licenses {
             license {
                 name = "Apache License, Version 2.0"
-                url = "https://raw.githubusercontent.com/hiero-ledger/$repoName/main/LICENSE"
+                url = "https://raw.githubusercontent.com/${gitHubOrg.get()}/$repoName/main/LICENSE"
             }
         }
 
         scm {
-            connection = "scm:git:git://github.com/hiero-ledger/$repoName.git"
-            developerConnection = "scm:git:ssh://github.com:hiero-ledger/$repoName.git"
-            url = "https://github.com/hiero-ledger/$repoName"
+            connection = "scm:git:git://github.com/${gitHubOrg.get()}/$repoName.git"
+            developerConnection = "scm:git:ssh://github.com:${gitHubOrg.get()}/$repoName.git"
+            url = "https://github.com/${gitHubOrg.get()}/$repoName"
         }
 
         developers {
